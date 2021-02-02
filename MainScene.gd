8e4c2c005096d95fd014e4473bda1e5183c188c0
Michael New <mikenew12@gmail.com>
2021-02-01 18:11:05 -0800

    show speed, high score, etc.

--- MainScene.gd
+++ MainScene.gd
@@ -1,30 +1,86 @@
 extends Node2D
 
 var Target = preload("res://target.tscn")
+onready var speed_label = get_node("speed_label")
+onready var best_speed_label = get_node("best_speed_label")
+
+var green = Color(181/255.0, 214/255.0, 61/255.0)
+var lightBlue = Color(0.02, 0.671, 0.294)
+var darkBlue = Color(0.004, 0.831, 0.271)
+var mediumBlue = Color(0.004, 0.282, 0.282)
+var yellow = Color(0.051, 0.298, 0.267)
+var orange = Color(0.059, 0.345, 0.384)
+var lightOrange = Color(0.059, 0.98, 0.894)
+var brown = Color(0.047, 0.153, 0.38)
+
 var accumulator = 0
 var lastSpawnTime = 0
-var spawnFrequency = 1.0
+var lastSpeedupTime = 0
+
+var startingFrequency = 1.0
+var spawnFrequency: float
 var targets = Array()
+
+var bestSpeed = 1.0
+var freezeSpeedLabel = false
 
 func _ready():
 	self.targets.push_back(self.spawn_target())
+	self.spawnFrequency = self.startingFrequency
 
-func spawn_target(_position=null):
+func spawn_target():
 	var t = Target.instance()
-	if !_position:
-		var vp = get_viewport_rect().size
-		var x = rand_range(0, vp.x)
-		var y = rand_range(0, vp.y)
-		t.position = Vector2(x, y)
+	var pad = t.maxRadius
+
+	var vp = get_viewport_rect().size
+	var x = rand_range(pad, vp.x - pad)
+	var y = rand_range(pad, vp.y - pad)
+	t.position = Vector2(x, y)
+	
 	add_child(t)
 	return t
 
 func _process(delta):
-	
 	self.accumulator += delta
+
 	if self.accumulator - self.lastSpawnTime > self.spawnFrequency:
 		self.targets.push_back(self.spawn_target())
 		self.lastSpawnTime = self.accumulator
+	
+	if self.accumulator - self.lastSpeedupTime > 1.0:
+		self.spawnFrequency *= .98
+		self.lastSpeedupTime = self.accumulator
+		if self.spawnFrequency < self.bestSpeed:
+			self.bestSpeed = self.spawnFrequency
+			self.speed_label.add_color_override("font_color", self.green)
+		self.updateLabel()
+		
+	for t in self.targets:
+		if t.dead: self.reset()
+
+func unfreezeSpeedLabel():
+	self.freezeSpeedLabel = false
+	
+func reset():
+	if self.spawnFrequency >= self.bestSpeed:
+		self.freezeSpeedLabel = true
+		get_tree().create_timer(2.0).connect("timeout", self, "unfreezeSpeedLabel")
+		get_tree().create_timer(2.0).connect("timeout", self, "resetCurrentSpeedColor")
+
+	self.spawnFrequency  = min(self.spawnFrequency * 2, self.startingFrequency)
+	self.updateLabel()
+
+	for t in self.targets: remove_child(t)
+	self.targets.clear()
+
+	self.accumulator = -1 # give an extra second after clearing the board
+	self.lastSpawnTime = 0
+	self.lastSpeedupTime = 0
+
+func updateLabel():
+	if !self.freezeSpeedLabel:
+		self.speed_label.text = str(int(1.0/self.spawnFrequency*100)/100.0) + " targets/second"
+	self.best_speed_label.text = str(int(1.0/self.bestSpeed*100)/100.0) + " (best)"
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
@@ -35,10 +91,7 @@ func _input(event):
 					self.targets.erase(t)
 					clickedTarget = true
 
-		if clickedTarget:
-			self.spawnFrequency *= .98
-		else:
-			self.spawnFrequency *= 2
-			for t in self.targets:
-				remove_child(t)
-				self.targets.erase(t)
+		if !clickedTarget: reset()
+	
+func resetCurrentSpeedColor():
+	self.speed_label.add_color_override("font_color", Color(1,1,1))
--- target.gd
+++ target.gd
@@ -1,24 +1,27 @@
 extends Node2D
 
-var radius = 32
-#var isClicked = false
-onready var sprite = get_node("Sprite")
+var radius: float
+var minRadius = 26
+var maxRadius = 42
+var lifeTime = 2.0
+var life = 0.0
+var dead = false
 
+onready var sprite = get_node("Sprite")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.scaleSprite()
+	self.radius = self.minRadius
 	
 func scaleSprite():
+	self.radius = self.minRadius + ((self.maxRadius - self.minRadius) * self.life / self.lifeTime)
 	var img_size = sprite.texture.get_size().x / 2
 	sprite.scale = Vector2(1, 1) * radius / img_size
 
-#func _input(event):
-#	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-#		if (event.position - self.position).length() < radius:
-#			self.isClicked = true
-#			print("click")
-
 func _process(delta):
-	radius += delta * 10
+	if self.life < self.lifeTime:
+		self.life += delta
+	else:
+		self.dead = true
 	self.scaleSprite()
